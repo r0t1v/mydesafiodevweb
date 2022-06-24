cb7cf2e7a77e4fb0c144de072093341a2f2478d8
vitor dikdama gabriel <vitormendes1999@hotmail.com>
2022-06-23 22:12:49 -0300

    project base

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # mydesafiodevweb
 
-Repository of job evaluation
+Repository for a test running for a job vacancy.
+
+For run the project, run in a server http our https.
+
+Challenge: [Desafio-dev-web](https://github.com/NikolasRoger/desafio-dev-web)
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+
+
+		<script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
+		<script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
+		<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+		<script src="like_button.js"></script>
+	</body>
+</html>
